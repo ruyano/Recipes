6497a2c46d82104f796207c032d8b2bcb6ea3715
ruyano <ruyano@gmail.com>
2020-03-06 12:20:48 -0400

    Melhorias na injeção de dependencias

--- app/src/main/java/br/com/udacity/ruyano/recipes/di/Injection.kt
+++ app/src/main/java/br/com/udacity/ruyano/recipes/di/Injection.kt
@@ -1,15 +1,10 @@
 package br.com.udacity.ruyano.recipes.di
 
-import br.com.udacity.ruyano.recipes.views.main.di.apiModule
-import br.com.udacity.ruyano.recipes.views.main.di.mainViewModelModule
-import br.com.udacity.ruyano.recipes.views.main.di.repositoryModule
+import br.com.udacity.ruyano.recipes.views.main.di.mainModule
 
 object Injection {
 
     val modules = listOf(
-            apiModule,
-            repositoryModule,
-            mainViewModelModule
+            mainModule
     )
-
 }
--- app/src/main/java/br/com/udacity/ruyano/recipes/views/main/di/MainModule.kt
+++ app/src/main/java/br/com/udacity/ruyano/recipes/views/main/di/MainModule.kt
@@ -5,12 +5,8 @@ import br.com.udacity.ruyano.recipes.networking.repositories.RecipeRepository
 import br.com.udacity.ruyano.recipes.views.main.MainViewModel
 import org.koin.dsl.module
 
-val apiModule = module {
+val mainModule = module {
     single { RetrofitConfig.getInstance().api }
-}
-val repositoryModule = module {
-    single { RecipeRepository(get()) }
-}
-val mainViewModelModule = module {
-    single { MainViewModel(get()) }
+    factory { RecipeRepository(get()) }
+    factory { MainViewModel(get()) }
 }
